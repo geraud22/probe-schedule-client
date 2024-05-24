b966183bb0eb0dbac1fb51056924b7ba2301bec9
Geraud Greyling <96425734+geraud22@users.noreply.github.com>
2024-05-24 13:31:04 +0200

    MakeRequests() refactor. Added README

---
+++ README.md
@@ -0,0 +1,29 @@
+Use client.py to conveniently authenticate with, and query endpoints of the ['Probe Schedule South Africa API'](https://probeschedule.co.za/).
+- Positional Arguments available:
+    - login
+    - farmlist
+    - blocklist
+    - devicelist
+    - devicestatus
+    - devicedata
+
+As with any Python project, it is recommended to run this tool in a Virtual Environment.
+Requires ['Python'](https://www.python.org/downloads/) to be installed locally.
+    - Create Virtual Environment (venv)
+    ```
+    python -m venv venv
+    ```
+    - Activate Virtual Environment
+    ```
+    venv/Scripts/activate
+    ```
+
+
+1. Install package dependencies
+```
+pip install -r requirements.txt
+```
+2. Run client.py using any defined positional argument. For example:
+```
+python client.py -h
+```
--- ps_client.py
+++ ps_client.py
@@ -6,27 +6,33 @@ class PS_CLient:
         this.farm_id = 0
         this.device_id = 0
         this.filename = ''
+        this.url = ''
     
     def __readToken(this):
-        # Returns String
         with open("token.json", 'r') as json_file:
             jsonObject = json.load(json_file)
             token = jsonObject['data']['token']
         return token
     
     def __makeJsonFile(this, response):
-        # Returns None
         jsonData = json.loads(response)
         with open(f"{this.filename}.json", 'w') as json_file:
             json.dump(jsonData, json_file, indent=4)
         print(f"Received Data has been saved to {this.filename}.json")
+        return None
         
     def __requestUserDetails(this):
-        # Returns a Dictionary
         userDetails = {}
         userDetails['username'] = input("Please enter your username: ")
         userDetails['password'] = input("Please enter your password: ")
         return userDetails
+    
+    def __makeRequest(this):
+        headers = {"authorization": f"Bearer {this.__readToken()}"}
+        r = requests.get(f"{this.url}", headers=headers)
+        response = r.content.decode('utf-8')
+        this.__makeJsonFile(response)
+        return None
     
     def login(this):
         this.filename = "token"
@@ -49,46 +55,34 @@ class PS_CLient:
     
     def getFarmList(this):
         this.filename = "farmlist"
-        headers = {
-            "authorization": f"Bearer {this.__readToken()}"
-            }
-        r = requests.get("https://api.probeschedule.co.za/data_api/v3/farms/list", headers=headers)
-        response = r.content.decode('utf-8')
-        this.__makeJsonFile(response)
+        this.url = "https://api.probeschedule.co.za/data_api/v3/farms/list"
+        this.__makeRequest()
         return None
     
     def getBlockList(this):
         this.filename = "blocklist"
         this.farm_id = input("Enter Farm ID: ")
-        headers = {"authorization": f"Bearer {this.__readToken()}"}
-        r = requests.get(f"https://api.probeschedule.co.za/data_api/v3/farms/{this.farm_id}/blocks", headers=headers)
-        response = r.content.decode('utf-8')
-        this.__makeJsonFile(response)
+        this.url = f"https://api.probeschedule.co.za/data_api/v3/farms/{this.farm_id}/blocks"
+        this.__makeRequest()
         return None
     
     def getDeviceList(this):
         this.filename = "devicelist"
         this.farm_id = input("Enter Farm ID: ")
-        headers = {"authorization": f"Bearer {this.__readToken()}"}
-        r = requests.get(f"https://api.probeschedule.co.za/data_api/v3/farms/{this.farm_id}/devices", headers=headers)
-        response = r.content.decode('utf-8')
-        this.__makeJsonFile(response)
+        this.url = f"https://api.probeschedule.co.za/data_api/v3/farms/{this.farm_id}/devices"
+        this.__makeRequest()
         return None
     
     def getDeviceStatus(this):
         this.filename = "devicestatus"
         this.device_id = input("Enter Device ID: ")
-        headers = {"authorization": f"Bearer {this.__readToken()}"}
-        r = requests.get(f"https://api.probeschedule.co.za/data_api/v3/devices/{this.device_id}/status", headers=headers)
-        response = r.content.decode('utf-8')
-        this.__makeJsonFile(response)
+        this.url = f"https://api.probeschedule.co.za/data_api/v3/devices/{this.device_id}/status"
+        this.__makeRequest()
         return None
     
     def getDeviceData(this):
         this.filename = "devicedata"
         this.device_id = input("Enter Device ID: ")
-        headers = {"authorization": f"Bearer {this.__readToken()}"}
-        r = requests.get(f"https://api.probeschedule.co.za/data_api/v3/devices/{this.device_id}/data", headers=headers)
-        response = r.content.decode('utf-8')
-        this.__makeJsonFile(response)
+        this.url = f"https://api.probeschedule.co.za/data_api/v3/devices/{this.device_id}/data"
+        this.__makeRequest()
         return None
